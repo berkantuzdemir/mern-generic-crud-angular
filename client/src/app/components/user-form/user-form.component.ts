6e3b99cfbacb04d08eca2ec1464823ec8554e1af
Furkan Karatas <frkn.krts.metu@gmail.com>
2022-08-04 15:02:31 +0300

    fix validation triggers

--- client/src/app/components/user-form/user-form.component.ts
+++ client/src/app/components/user-form/user-form.component.ts
@@ -96,4 +96,20 @@ export class UserFormComponent implements OnInit {
     });
   }
 
+  onSubmit() {
+    console.log(this.userForm);
+    this.validateAllFormFields(this.userForm);
+  }
+
+  validateAllFormFields(formGroup: FormGroup) {         //{1}
+    Object.keys(formGroup.controls).forEach(field => {  //{2}
+      const control = formGroup.get(field);             //{3}
+      if (control instanceof FormControl) {             //{4}
+        control.markAsTouched({ onlySelf: true });
+      } else if (control instanceof FormGroup) {        //{5}
+        this.validateAllFormFields(control);            //{6}
+      }
+    });
+  }
+
 }
