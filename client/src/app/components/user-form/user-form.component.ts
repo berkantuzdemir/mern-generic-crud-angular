89cc31c28fd78ed7166efbf5918f2feadeb9fec2
Furkan Karatas <frkn.krts.metu@gmail.com>
2022-08-04 14:16:11 +0300

    add bootstrap and form validations

--- client/src/app/components/user-form/user-form.component.ts
+++ client/src/app/components/user-form/user-form.component.ts
@@ -1,5 +1,11 @@
 import { Component, OnInit } from '@angular/core';
-import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import {
+  FormBuilder,
+  FormGroup,
+  Validators,
+  FormControl
+} from '@angular/forms';
+
 
 @Component({
   selector: 'app-user-form',
@@ -14,28 +20,74 @@ export class UserFormComponent implements OnInit {
 
   constructor(public fb: FormBuilder) {}
 
-  
-
-  get positionName() {
-    return this.userForm.get('positionName');
-  }
-
   ngOnInit(): void {
     this.userForm = this.fb.group({
-      name: null,
-      email: null,
-      photo: null,
-      startDate: null,
-      positionName: null,
-      department: null,
-      university: null,
-      graduation: null,
-      previousJob: null,
-      previousPosition: null,
-      totalExperience: null,
-      technicalSkills: null,
-      about: null
+      name: [null, Validators.required],
+      email: [null, Validators.required],
+      photo: [null, Validators.required],
+      startDate: [null, Validators.required],
+      positionName: [null, Validators.required],
+      department: [null, Validators.required],
+      university: [null, Validators.required],
+      graduation: [null, Validators.required],
+      previousJob: [null, Validators.required],
+      previousPosition: [null, Validators.required],
+      totalExperience: [null, Validators.required],
+      technicalSkills: [null, Validators.required, Validators.minLength(20)],
+      about: [null, Validators.required, Validators.minLength(150)]
     });
+  }
+
+  get name(): FormControl {
+    return this.userForm.get('name') as FormControl;
+  }
+
+  get email(): FormControl {
+    return this.userForm.get('email') as FormControl;
+  }
+
+  get photo(): FormControl {
+    return this.userForm.get('photo') as FormControl;
+  }
+
+  get startDate(): FormControl {
+    return this.userForm.get('startDate') as FormControl;
+  }
+
+  get positionName(): FormControl {
+    return this.userForm.get('positionName') as FormControl;
+  }
+
+  get department(): FormControl {
+    return this.userForm.get('department') as FormControl;
+  }
+
+  get university(): FormControl {
+    return this.userForm.get('university') as FormControl;
+  }
+
+  get graduation(): FormControl {
+    return this.userForm.get('graduation') as FormControl;
+  }
+
+  get previousJob(): FormControl {
+    return this.userForm.get('previousJob') as FormControl;
+  }
+
+  get previousPosition(): FormControl {
+    return this.userForm.get('previousPosition') as FormControl;
+  }
+
+  get totalExperience(): FormControl {
+    return this.userForm.get('totalExperience') as FormControl;
+  }
+
+  get technicalSkills(): FormControl {
+    return this.userForm.get('technicalSkills') as FormControl;
+  }
+
+  get about(): FormControl {
+    return this.userForm.get('about') as FormControl;
   }
 
   changePosition(e: any) {
