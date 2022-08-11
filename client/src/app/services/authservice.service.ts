4632757e312aa8c84d7dd0c4b1574887807da12c
berkantuzdemir <36998997+berkantuzdemir@users.noreply.github.com>
2022-08-11 16:08:33 +0300

    budgets max value changed

--- client/src/app/services/authservice.service.ts
+++ client/src/app/services/authservice.service.ts
@@ -9,7 +9,7 @@ import { retry, catchError } from 'rxjs/operators';
 })
 export class AuthserviceService {
   Authdata!: any;
-  apiURL = 'http://localhost:4200';
+  apiURL = 'https://mern-generic-crud-angularr.herokuapp.com/';
   headers = new HttpHeaders().set('Content-Type', 'application/json');
   currentUser = {};
 
