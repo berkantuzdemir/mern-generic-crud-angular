d5cf802544b7eda0e1dbf1ea2c9cc3ff92fb14c4
Berna Ikiz <berna.ikiz@mera.com>
2022-08-18 15:35:03 +0300

    Delete operation is provided for user list table

--- client/src/app/components/user-list/user-list.component.ts
+++ client/src/app/components/user-list/user-list.component.ts
@@ -1,8 +1,8 @@
 import { AfterViewInit, Component, DoCheck, OnChanges, OnInit, ViewChild } from '@angular/core';
 import { MatDialog } from '@angular/material/dialog';
 import { MatPaginator } from '@angular/material/paginator';
-import { MatSort,MatSortModule, Sort } from '@angular/material/sort';
-import { MatTableDataSource, MatTableModule} from '@angular/material/table';
+import { MatSort, MatSortModule, Sort } from '@angular/material/sort';
+import { MatTableDataSource, MatTableModule } from '@angular/material/table';
 import { AuthserviceService } from 'src/app/services/authservice.service';
 import { User } from '../../model/user';
 import { PopupListComponent } from '../popup-list/popup-list.component';
@@ -13,53 +13,53 @@ import { PopupListComponent } from '../popup-list/popup-list.component';
   styleUrls: ['./user-list.component.css']
 })
 
-export class UserListComponent implements OnInit, AfterViewInit  {
+export class UserListComponent implements OnInit, AfterViewInit {
 
-   data: User[] = [
-    {fullname: 'berkant', department: 'Hydrogen', position: 'asdasdas', university: 'fdsdf', firstJobDay: '2022-08-11T00:00:00.000Z', email: 'fdsdf', description: 'fdsdf', createdAt: 'fdsdf', graduationTime: 'fdsdf', image: 'fdsdf', previousJob: 'fdsdf', previousWorkTitle: 'sadasd', skills:'asdas', totalWorkTime: 'aasdasd', workTitle: 'asdasdsa', __v:'0', _id: '12312' },
-    {fullname: 'berka1nt', department: 'Sydro1gen', position: 'asda1sdas', university: 'fddf', firstJobDay: '2022-08-12T00:00:00.000Z', email: 'fdf', description: 'fdf', createdAt: 'fdf', graduationTime: 'fdf', image: 'fdf', previousJob: 'fdf', previousWorkTitle: 'dasd', skills:'aas', totalWorkTime: 'adasd', workTitle: 'asddsa', __v:'1', _id: '1212' },
+  data: User[] = [
+    { fullname: 'berkant', department: 'Hydrogen', position: 'asdasdas', university: 'fdsdf', firstJobDay: '2022-08-11T00:00:00.000Z', email: 'fdsdf', description: 'fdsdf', createdAt: 'fdsdf', graduationTime: 'fdsdf', image: 'fdsdf', previousJob: 'fdsdf', previousWorkTitle: 'sadasd', skills: 'asdas', totalWorkTime: 'aasdasd', workTitle: 'asdasdsa', __v: '0', _id: '12312' },
+    { fullname: 'berka1nt', department: 'Sydro1gen', position: 'asda1sdas', university: 'fddf', firstJobDay: '2022-08-12T00:00:00.000Z', email: 'fdf', description: 'fdf', createdAt: 'fdf', graduationTime: 'fdf', image: 'fdf', previousJob: 'fdf', previousWorkTitle: 'dasd', skills: 'aas', totalWorkTime: 'adasd', workTitle: 'asddsa', __v: '1', _id: '1212' },
   ];
-  displayedColumns = ['fullname', 'department', 'position','university'];
+  displayedColumns = ['checked', 'fullname', 'department', 'position', 'university'];
   clickedRows = new Set<User>();
-  dataSource! : MatTableDataSource<User>;
+  dataSource!: MatTableDataSource<User>;
   user!: User[]
   @ViewChild(MatPaginator) paginator!: MatPaginator;
   @ViewChild(MatSort) sort!: MatSort;
 
-  constructor(private Auth: AuthserviceService, private dialogRef : MatDialog) { }
-    openDialog(row:any){
-      this.clickedRows.add(row);
-      this.dialogRef.open(PopupListComponent);
-    }
+  constructor(private Auth: AuthserviceService, private dialogRef: MatDialog) { }
+  openDialog(row: any) {
+    this.clickedRows.add(row);
+    this.dialogRef.open(PopupListComponent);
+  }
 
 
   ngOnInit(): void {
-  this.getUsers();
+    this.getUsers();
   }
 
   getUsers() {
-  //   this.Auth.getUser().subscribe(data => {
-  //     let result = data.map((obj:User)=>{
-  //       return  {
-  //         'fullname':obj.fullname,
-  //         'department':obj.department,
-  //         'position':obj.workTitle,
-  //         'university':obj.university
-  //        }
-  //     })
-  //     this.dataSource = result
-  //     console.log('brn result',result)
-  //     console.log('brn',this.dataSource)
-  //     this.dataSource.paginator = this.paginator;
-  //     this.dataSource.sort = this.sort;
-  //  })
+    //   this.Auth.getUser().subscribe(data => {
+    //     let result = data.map((obj:User)=>{
+    //       return  {
+    //         'fullname':obj.fullname,
+    //         'department':obj.department,
+    //         'position':obj.workTitle,
+    //         'university':obj.university
+    //        }
+    //     })
+    //     this.dataSource = result
+    //     console.log('brn result',result)
+    //     console.log('brn',this.dataSource)
+    //     this.dataSource.paginator = this.paginator;
+    //     this.dataSource.sort = this.sort;
+    //  })
 
 
-  this.dataSource = new MatTableDataSource<User>(this.data)
-  this.dataSource.paginator = this.paginator;
-  this.dataSource.sort = this.sort;
-  console.log(this.dataSource)
-   }
+    this.dataSource = new MatTableDataSource<User>(this.data)
+    this.dataSource.paginator = this.paginator;
+    this.dataSource.sort = this.sort;
+    console.log(this.dataSource)
+  }
 
   applyFilter(filterValue: string) {
     filterValue = filterValue.trim(); // Remove whitespace
@@ -68,16 +68,21 @@ export class UserListComponent implements OnInit, AfterViewInit  {
   }
 
   ngAfterViewInit(): void {
-      this.dataSource.sort = this.sort;
+    this.dataSource.sort = this.sort;
+  }
+
+  delete(){
+    this.dataSource.data = this.dataSource.data.filter((data: any) => !this.clickedRows.has(data));
   }
 
   sortData(sort: Sort) {
-    if (!sort.active || sort.direction === '' || this.dataSource.sort ) {
+    if (!sort.active || sort.direction === '' || this.dataSource.sort) {
       (this.sort as any) = this.dataSource.sort;
       return;
     }
 
-    this.sort  = (this.dataSource.sort as any)((a:User, b:User) => {
+
+    this.sort = (this.dataSource.sort as any)((a: User, b: User) => {
       const isAsc = sort.direction === 'asc';
       switch (sort.active) {
         case 'fullname':
@@ -95,6 +100,6 @@ export class UserListComponent implements OnInit, AfterViewInit  {
   }
 }
 
-  function compare(a: number | string, b: number | string, isAsc: boolean) {
+function compare(a: number | string, b: number | string, isAsc: boolean) {
   return (a < b ? -1 : 1) * (isAsc ? 1 : -1);
- }
+}
